cdef2164d143a78fe65a9a9eb1b36f30a05f54cd
jordantolin <138111277+jordantolin@users.noreply.github.com>
2025-02-14 09:23:53 +0100

    Delete BubbleTrouble directory

--- BubbleTrouble/client/src/hooks/use-upload.ts
+++
@@ -1,5 +0,0 @@
-import { generateReactHelpers } from "@uploadthing/react";
-import type { OurFileRouter } from "../../server/uploadthing";
- 
-export const { useUploadThing, uploadFiles } =
-  generateReactHelpers<OurFileRouter>();
